720dfb62cf5ddab8dd31298a7eb686ae88b6588e
Murat Bayram <m.byram@gmail.com>
2017-04-03 23:33:40 +0300

    random number generating

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Darwin
 
 struct guess {
     var number = 0;
@@ -15,9 +16,12 @@ struct guess {
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var gloText: UITextField!
     @IBOutlet var myText: UITextField!
+    @IBOutlet var countText: UITextField!
     @IBOutlet weak var myTableView: UITableView!
-    
+    var globalIndex = 50
+    var globalSum = 0
     
     var guessHistory = [guess]()
     
@@ -31,22 +35,60 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
 
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent!) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent!) {
         self.view.endEditing(true)
     }
     
-    @IBAction func btnClick(sender: UIButton){
-        guessHistory.append(guess(number: Int(myText.text!)!, result: "test"))
-        self.view.endEditing(true)
-        myTableView.reloadData()
+    @IBAction func btnClick(_ sender: UIButton){
+        var sum = 0
+        globalIndex = 50
+        globalSum = 0
+        guessHistory.removeAll()
+        
+        var cc = Int(countText.text!)
+        let quene  = DispatchQueue(label: "asd")
+        
+        quene.async {
+            
+            for i in 0...Int(cc!){
+                sum = 0;
+                for j in 0...100 {
+                    var randa = Int(arc4random_uniform(101))
+                    sum = sum + randa
+                }
+                
+                var avg = Int (sum/100)
+
+                self.globalSum = self.globalSum + avg
+                self.globalIndex = self.globalIndex + (avg - 50)
+                
+                self.guessHistory.append(guess(number: avg, result: "test"))
+                
+                
+                DispatchQueue.main.async {
+                    self.myText.text = String(self.globalSum/(i+1))
+                    self.myTableView.reloadData()
+                    self.gloText.text = String(self.globalIndex)
+                }
+                
+                usleep(400000)
+                
+            }
+            
+            
+            
+        }
+        
         
     }
     
-    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return guessHistory.count
     }
     
-    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         
         let cell = UITableViewCell();
         cell.textLabel?.text = String(guessHistory[indexPath.row].number);
